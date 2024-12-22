161d9636bc70ba4312c0185c599119b7574619f8
21anhn <ltnanh21@gmail.com>
2024-12-22 18:18:11 +0700

    [Add] Add input check

---
+++ Socialix/Common/Validators/InputCheck.cs
@@ -0,0 +1,51 @@
+﻿using Socialix.Common.API;
+using Socialix.Common.Constants;
+
+namespace Socialix.Common.Validators
+{
+    public class InputCheck<T> where T : class
+    {
+        public void CheckRequired(InputField<T> inputField, List<DetailError> detailErrors)
+        {
+            if (typeof(T) == typeof(string))
+            {
+                if (string.IsNullOrEmpty(inputField.Value.ToString()))
+                {
+                    detailErrors.Add(new DetailError
+                    {
+                        FieldName = inputField.FieldName,
+                        Value = inputField.Value.ToString(),
+                        MessageId = Message.E00005,
+                        Message = Message.GetMessageById(Message.E00005)
+                    });
+                }
+            }
+            else if (Nullable.GetUnderlyingType(typeof(T)) != null) // Check nullable datatype int?, object?,...
+            {
+                if (inputField.Value == null)
+                {
+                    detailErrors.Add(new DetailError
+                    {
+                        FieldName = inputField.FieldName,
+                        Value = null,
+                        MessageId = Message.E00005,
+                        Message = Message.GetMessageById(Message.E00005)
+                    });
+                }
+            }
+            else
+            {
+                if (EqualityComparer<T>.Default.Equals(inputField.Value, default(T))) // Check default datatype int,...
+                {
+                    detailErrors.Add(new DetailError
+                    {
+                        FieldName = inputField.FieldName,
+                        Value = inputField?.Value?.ToString(),
+                        MessageId = Message.E00005,
+                        Message = Message.GetMessageById(Message.E00005)
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ Socialix/Common/Validators/InputField.cs
@@ -0,0 +1,8 @@
+﻿namespace Socialix.Common.Validators
+{
+    public class InputField<T> where T : class
+    {
+        public string FieldName { get; set; }   
+        public T Value { get; set; }
+    }
+}
